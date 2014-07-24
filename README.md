0c65a94fd1787cad6c237bf7ca46ebc037cf8d6d
dboissin <damien.boissin@web-education.net>
2014-07-24 13:31:08 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# WebDav module
+
---
+++ src/main/java/fr/wseduc/webdav/WebDav.java
@@ -0,0 +1,128 @@
+package fr.wseduc.webdav;
+
+import com.github.sardine.Sardine;
+import com.github.sardine.SardineFactory;
+import com.github.sardine.impl.SardineImpl;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.ssl.*;
+import org.vertx.java.busmods.BusModBase;
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.json.JsonObject;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.security.*;
+
+public class WebDav extends BusModBase implements Handler<Message<JsonObject>> {
+
+	private JsonObject credentials;
+
+	@Override
+	public void start() {
+		super.start();
+		credentials = config.getObject("credentials", new JsonObject());
+		vertx.eventBus().registerHandler(config.getString("address", "webdav"), this);
+	}
+
+	@Override
+	public void handle(Message<JsonObject> message) {
+		String action = message.body().getString("action", "");
+		switch (action) {
+			case "put":
+				put(message);
+				break;
+			default:
+				sendError(message, "Invalid action.");
+		}
+	}
+
+	private void put(Message<JsonObject> message) {
+		String filePath = message.body().getString("file");
+		if (filePath == null || filePath.trim().isEmpty()) {
+			sendError(message, "Invalid file.");
+			return;
+		}
+		FileInputStream fis;
+		try {
+			fis = new FileInputStream(filePath);
+		} catch (FileNotFoundException e) {
+			sendError(message, "Invalid file.", e);
+			return;
+		}
+		String uri = message.body().getString("uri");
+		Sardine sardine = getSardine(uri, message);
+		if (sardine == null) return;
+		try {
+			sardine.put(uri, fis);
+			sendOK(message);
+		} catch (IOException e) {
+			sendError(message, e.getMessage(), e);
+		}
+	}
+
+	private Sardine getSardine(String uri, Message<JsonObject> message) {
+		String host;
+		try {
+			host = new URI(uri).getHost();
+		} catch (URISyntaxException e) {
+			sendError(message, e.getMessage(), e);
+			return null;
+		}
+		JsonObject credential = credentials.getObject(host);
+		Sardine sardine;
+		if (credential != null) {
+			if (credential.getBoolean("insecure", false)) {
+				sardine = new SardineImpl() {
+					@Override
+					protected ConnectionSocketFactory createDefaultSecureSocketFactory() {
+						SSLConnectionSocketFactory sf = null;
+						TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+
+							@Override
+							public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+								return null;
+							}
+
+							@Override
+							public void checkClientTrusted(java.security.cert.X509Certificate[] certs,
+									String authType) {
+							}
+
+							@Override
+							public void checkServerTrusted(java.security.cert.X509Certificate[] certs,
+									String authType) {
+							}
+						} };
+						try {
+						SSLContext context = SSLContext.getInstance("TLS");
+						context.init(null, trustAllCerts, null);
+
+						 sf = new SSLConnectionSocketFactory(context,
+								 SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+						} catch (NoSuchAlgorithmException | KeyManagementException e) {
+							logger.error(e.getMessage(), e);
+						}
+						return sf;
+					}
+				};
+				sardine.setCredentials(
+						credential.getString("username"), credential.getString("password"));
+			} else {
+				sardine = SardineFactory.begin(
+						credential.getString("username"), credential.getString("password"));
+			}
+			sardine.enablePreemptiveAuthentication(host);
+		} else {
+			sardine = SardineFactory.begin();
+		}
+		return sardine;
+	}
+
+}
