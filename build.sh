85201313598f87ccd643eca097ef49c82ff615ad
Junior Bernard <junior.bernard@opendigitaleducation.com>
2026-04-17 13:10:23 +0200

    ci: fix maven options

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ publish () {
     *)         export nexusRepository='releases' ;;
   esac
 
-  docker compose run --rm  maven mvn -DrepositoryId=ode-$nexusRepository -DskiptTests --settings /var/maven/.m2/settings.xml deploy
+  docker compose run --rm  maven mvn $MVN_OPTS -DrepositoryId=ode-$nexusRepository -DskiptTests --settings /var/maven/.m2/settings.xml deploy
 }
 
 test () {
